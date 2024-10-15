5f90f13275ce9d5d611885b3859d2b51f9aeffb5
Ian Kabaka <iankabaka1@gmail.com>
2024-10-15 13:22:52 +0300

    adding images to DOM

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  let div = document.querySelector("div");
+  fetch("http://localhost:3000/sneakers")
+    .then((res) => res.json())
+    .then((data) =>
+      data.forEach((Obj) => {
+        images = document.createElement("img");
+        images.src = Obj.image;
+        div.appendChild(images);
+      })
+    );
+});
